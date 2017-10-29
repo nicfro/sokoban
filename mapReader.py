7e0a71f205d0eeb93e76f0d2005ae2acef208e6f
Arkimedes af Syrakus <arkimedes@osm15>
2017-10-29 20:49:23 +0100

    Added Linux support

--- mapReader.py
+++ mapReader.py
@@ -4,7 +4,7 @@ import platform
 
 line_ending = '\r\n'
 
-if (platform.system() == 'Darwin'): #Fixes Yuan's problem
+if (platform.system() == 'Darwin' or platform.system() == 'Linux'): #Fixes Yuan's problem
     line_ending = '\n'
 
 
