8e66f19b4a97be2eaa9f608d44539f0b84b0a4f9
Nicolai Jacobsen <NJacobsen@dk.imshealth.com>
2017-12-18 23:25:15 +0900

    added padding to mapgeneration

--- mapGeneration.py
+++ mapGeneration.py
@@ -160,6 +160,14 @@ def constructNewMap(dimx,dimy):
                     
     return newMap
 
-
-for i in range(10):
-    print(constructNewMap(3,4))
+x = 2
+y = 3
+generatedMap = []
+for i in range(1000):
+    res = constructNewMap(x,y)
+    if  res != 0:
+        res = np.vstack((res,["#"]*y*3))
+        res = np.vstack((["#"]*y*3, res))
+        res = np.hstack((res,np.array(["#"]*((x*3)+2)).reshape(-1,1)))
+        res = np.hstack((np.array(["#"]*((x*3)+2)).reshape(-1,1), res))
+        generatedMap.append(res)
